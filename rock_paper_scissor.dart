1e58e7d47d329a4df3a3d5c040f8ae474da36c1d
VadimV3 <myadwordclient@gmail.com>
2025-04-09 17:19:54 +0300

    The changes are coherent with the revisions to the revision pool.

--- rock_paper_scissor.dart
+++ rock_paper_scissor.dart
@@ -13,19 +13,20 @@ void main() {
 
 enum GameElements { rock, paper, scissor }
 
-late String userName;
-late GameElements? userChoice;
-late GameElements compChoice;
+String userName = '';
+GameElements? userChoice = GameElements.scissor;
+GameElements compChoice = GameElements.scissor;
 
 var userScore = 0;
 var compScore = 0;
 
 //Flow
 void gameFlow() {
-  while (userChooseHandler()) {
+  // ! Can i write like this? Function in Function
+  while (userInputHandler(userInputValidation())) {
     //Comp pick item
     getRandomGameElement();
-    winConditions(userChoice, compChoice);
+    defineWinner(userChoice, compChoice);
   }
   print('Game Over!');
 }
@@ -44,15 +45,21 @@ void addUserName() {
   }
 }
 
-bool userChooseHandler() {
+int userInputValidation() {
   print('$userName write  the number to choose element or input 9 to exit game');
   for (var i = 0; i < GameElements.values.length; i++) {
     print('${i + 1} - ${GameElements.values[i].name}');
   }
   String? userPick = stdin.readLineSync();
   int? choosenNumber = int.tryParse(userPick ?? '');
+  if (choosenNumber == null) {
+    choosenNumber = 0;
+  }
+  return choosenNumber;
+}
 
-  switch (choosenNumber) {
+bool userInputHandler(int chooseNumber) {
+  switch (chooseNumber) {
     case 1:
       userChoice = GameElements.rock;
       break;
@@ -72,7 +79,7 @@ bool userChooseHandler() {
   return true;
 }
 
-void winConditions(GameElements? userInput, GameElements compChoice) {
+void defineWinner(GameElements? userInput, GameElements compChoice) {
   if (userInput != null) {
     userChoice = userInput;
     //Draw Win Check
@@ -80,7 +87,7 @@ void winConditions(GameElements? userInput, GameElements compChoice) {
       print('A draw win');
       return;
     }
-    //USer Win
+    //User Win
     if ((userChoice == GameElements.rock && compChoice == GameElements.scissor) ||
         (userChoice == GameElements.paper && compChoice == GameElements.rock) ||
         (userChoice == GameElements.scissor && compChoice == GameElements.paper)) {
@@ -91,13 +98,13 @@ void winConditions(GameElements? userInput, GameElements compChoice) {
       print('Computer Win!');
       compScore++;
     }
-    results();
+    printResults();
   } else {
     print('Check input');
   }
 }
 
-void results() {
+void printResults() {
   String userPick = userChoice.toString().split('.').last;
   String compPick = compChoice.toString().split('.').last;
 
