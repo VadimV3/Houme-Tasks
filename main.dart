ce47c79913a910d36643ef1678fe132d9ecc45e2
VadimV3 <myadwordclient@gmail.com>
2025-04-15 18:17:50 +0300

    Working Version 1

--- main.dart
+++ main.dart
@@ -1,3 +1,33 @@
 void main() {
-  print('hello');
+  int firstDetailCount = findDetailByRegularExp(RegExp(r'\*\.'), inputData);
+  int secondDetailCount = findDetailByRegularExp(RegExp(r'\:'), inputData);
+  int phoneCount = phonefactory(firstDetailCount, secondDetailCount);
+  print('You will get $phoneCount phones');
+}
+
+const inputData = [
+  '*.**::::**..*::.*…**.*::',
+  '*.**::::**..*::.*…**.*::',
+  '*.**::::**..*::.*…**.*::',
+];
+
+int findDetailByRegularExp(RegExp pattern, List<String> data) {
+  int count = 0;
+  RegExp regExp = pattern;
+
+  for (String part in data) {
+    count += regExp.allMatches(part).length;
+  }
+  return count;
+}
+
+int phonefactory(int countOffirstDetails, int countOfSecondDetails) {
+  int needFirstDetails = 3;
+  int needSecondDetails = 4;
+
+  int result =
+      ((countOffirstDetails + countOfSecondDetails) / (needSecondDetails + needFirstDetails))
+          .floor();
+
+  return result;
 }
