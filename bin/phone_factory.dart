54e1c2aaf618ec74fdebcdc444c68d1fa8d739cf
saveno2 <saveno2@medtronic.com>
2025-04-27 20:37:44 +0300

    test coverage added

--- bin/phone_factory.dart
+++
@@ -1,5 +0,0 @@
-import 'package:phone_factory/phone_factory.dart' as phone_factory;
-
-void main(List<String> arguments) {
-  print('Hello world: ${phone_factory.calculate()}!');
-}
--- test/phone_factory_test.dart
+++ test/phone_factory_test.dart
@@ -1,12 +1,11 @@
+import 'package:phone_factory/phone_factory.dart' show findDetail;
 import 'package:test/test.dart';
-
-import '../main.dart';
 
 void main() {
   group('Find Details', () {
     test('Return 0 of second part', () {
       final input = ['**.', '.**'];
-      expect(findDetail(input, '*.', 2), 0);
+      expect(findDetail(input, '*.', ''), 0);
     });
 
     test('Return count of second part', () {
